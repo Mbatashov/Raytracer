0a3553a507b1e31828a26be1cb6d488cd9a18657
Mbatashov <119846570+Mbatashov@users.noreply.github.com>
2025-08-30 19:47:28 -0400

    Add files via upload

---
+++ color.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H 
+
+#include "vec3.h" 
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x(); 
+    auto y = pixel_color.y(); 
+    auto z = pixel_color.z(); 
+
+    int rbyte = int(255.999 * r); 
+    int gbyte = int(255.999 * y);
+    int bbyte = int(255.999 * z);
+
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
---
+++ vec3.h
@@ -0,0 +1,100 @@
+#ifndef VEC3_H
+#define VEC3_H 
+#include <cmath>
+#include <iostream>
+class vec3 {
+    public: 
+        double e[3]; 
+        // use initialiser lists. Const data members must be initialised this way. also more efficient and useful for things without default constructors.  also base classes and inheritance
+        vec3(): e{0,0,0} {}; 
+        vec3(double e0, double e1, double e2) : e{e0,e1,e2} {}; 
+
+        //const keyword indicates that the state of the object( or vector) is not modified, ie value of data members cannot change in the function
+        double x() const {return e[0];}
+        double y() const {return e[1];}
+        double z() const {return e[2];}
+        
+        // operator overloading
+        vec3 operator-() const {return vec3(-e[0],-e[1],-e[2]);}
+        double operator[](int i) const {return e[i];}
+        // sometimes ampersand operator needed for when we need to screw with an original data member and not a copy
+        double& operator[](int i) {return e[i];}
+
+        vec3& operator+=(const vec3& v){
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        vec3& operator*=(double t){
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& operator/=(double t){
+            return *this *= 1/t; 
+        }
+
+        double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }   
+
+        double length() const {
+            return std::sqrt(length_squared());
+        }
+};
+
+//we denote point 3 as an alias to vec3 
+using point3 = vec3; 
+
+// vector utility functions; 
+//write out the function
+//inline, funciton is short, have it be compiled 'inline' and saving us the overhead of having to call its location on the stack
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2]; 
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v){
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v){
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v){
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v){
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t){
+    return t * v; 
+}
+
+inline vec3 operator/( const vec3& v, double t){
+    return (1/t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0] 
+    + u.e[1] * v.e[1] +
+    u.e[2] * v.e[2]; 
+}
+
+inline vec3 cross(const vec3& u, const vec3& v)
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3& v){
+    return v/v.length();
+}
+#endif
